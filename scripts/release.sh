f3f8ca8b11620037aaeb7b2706cf74ac2f47b708
Juan Carrillo <inetshell@gmail.com>
2022-04-05 07:15:50 -0500

    Refactored release.sh script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # https://helm.sh/docs/topics/chart_repository/
 helm repo index ../releases
-gsutil rsync -r ../releases/ gs://hobops-helm-charts/
+gsutil cp "../releases/*.tgz" gs://hobops-helm-charts/
+gsutil -h "Content-Type:text/html" -h "Cache-Control:public, max-age=0" cp ../releases/index.yaml gs://hobops-helm-charts/index.yaml
